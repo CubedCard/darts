34d1a1cfd0d333c0fee1eed7be8467a4d30d7494
J. Derksen <jip.derksen@hva.nl>
2024-10-02 15:26:04 +0200

    Create home page

---
+++ darts-fe/src/app/components/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Assuming you'll navigate to a new game component
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  startGame() {
+    // Navigate to the game component, assuming you have a route for it
+    this.router.navigate(['/game']);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
